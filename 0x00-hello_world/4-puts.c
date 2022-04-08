ae702a7bfd95942c66e9437d22531baa4631df5c
Solomon <johnslon8@gmail.com>
2022-04-07 18:13:45 -0700

    hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #imclude <stdio.h>
 /**
- * main - entry
+ * main - entry point
  *
  * return: 0 after printing the function
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+  puts("\"Programming is like building a multilingual puzzle");
+  return (0);
 }
