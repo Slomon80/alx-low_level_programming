b7e090dca76e7b25f192119bc3024778e72bdc9a
Solomon <johnslon8@gmail.com>
2022-04-07 18:09:22 -0700

    hello puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#imclude <stdio.h>
+/**
+ * main - entry
+ *
+ * return: 0 after printing the function
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
