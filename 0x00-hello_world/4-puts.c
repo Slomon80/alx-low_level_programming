c61017769d009f93b448c629904db20f82c7ebeb
Solomon <johnslon8@gmail.com>
2022-04-07 19:37:29 -0700

    puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,20 @@
-#imclude <stdio.h>
-/**
- * main - entry point
- *
- * return: 0 after printing the function
- */
-int main(void)
-{
-  puts("\"Programming is like building a multilingual puzzle");
-  return (0);
+#include <stdio.h>                                                                                                                                                        
+
+/**                                                                                                                                                                       
+
+ *main - Entry point                                                                                                                                                      
+
+ *                                                                                                                                                                        
+
+ *Return: 0 after printing the function                                                                                                                                   
+
+ */                                                                                                                                                                       
+
+int main(void)                                                                                                                                                            
+
+{                                                                                                                                                                         
+
+puts(*\*Pprogramming is like building a multilingual puzzle");                                                                                                            
+
+retun (0);
 }
