50606f9c73f659342c5b365c2e7a380dac3a7af7
Solomon <johnslon8@gmail.com>
2022-04-14 15:48:29 -0700

    task 1

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -1,11 +1,7 @@
 #include <unistd.h>
-/**
- * _putchar - write c to stand out
- * @c: The character to print
- *
- * Return: 0 successful
- */
+
 int _putchar(char c)
 {
 	return (write(1, &c, 1));
 }
+	
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,7 +1,7 @@
 #ifndef MAIN_H_INCLUDE
 #define MAIN_H_INCLUDE
 int _putchar(char c);
-void print_alphabet_x10(void);
+void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
