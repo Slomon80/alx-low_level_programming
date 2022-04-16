aa29a53c7483d212780625cf0ca0ad4f5a99afc2
Solomon <johnslon8@gmail.com>
2022-04-16 08:27:21 -0700

    Task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_last_digit - to print thhe last digit
+ * @num: the character to be tested.
+ */
+int print_last_digit(int num)
+{
+	if (num < 0)
+	{
+		num = num * -1;
+	}
+	_putchar((num % 10) + '0');
+	return (num % 10);
+}
