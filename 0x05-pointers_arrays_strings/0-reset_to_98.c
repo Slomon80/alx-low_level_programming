cd9a459894b213bb62952e99c9e513e9147a7370
Solomon <johnslon8@gmail.com>
2022-04-20 20:06:58 -0700

    Task 0, pointers and arrays

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - contains pointer parameter to be updated.
+ * @n: parameter to be updated
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
